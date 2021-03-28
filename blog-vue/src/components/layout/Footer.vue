4baa9e9103a89d7f584e44f4d4c3fdf6a2b0322b
CodePrometheus <Code_Prince@outlook.com>
2021-03-28 14:40:18 +0800

    页脚变色处理

--- blog-vue/src/components/layout/Footer.vue
+++ blog-vue/src/components/layout/Footer.vue
@@ -1,16 +1,54 @@
 <template>
-  <v-footer app padless absolute style="background:rgba(0,0,0,0)">
-    <v-col class="text-center footer" cols="12">
-      <div>Copyright &copy; 2020-{{ new Date().getFullYear()   }}</div>
-    </v-col>
+  <v-footer app padless absolute v-if="!this.isMessage">
+    <div class="footer-wrap">
+      <div>&copy; 2020-{{ new Date().getFullYear() }} By zzStar</div>
+      <div>
+        <a target="_blank" href="https://github.com/CodePrometheus"
+           class="mr-9 iconfont icongithub"/>
+        <a target="_blank" href="https://space.bilibili.com/342251858"
+           class="iconfont icon-bilibili-fill"/>
+      </div>
+    </div>
   </v-footer>
 </template>
 
-<style scoped>
-.footer {
-  font-size: 0.75rem;
+<script>
+export default {
+  computed: {
+    isMessage() {
+      return this.$route.path == "/message"
+    }
+  }
 }
-.footer div {
-  line-height: 1.75;
+</script>
+
+<style scoped>
+.footer-wrap {
+  width: 100%;
+  line-height: 2;
+  position: relative;
+  padding: 40px 20px;
+  color: #eee;
+  font-size: 14px;
+  text-align: center;
+  background: linear-gradient(-45deg, #ee7752, #ce3e75, #23a6d5, #a871d6, #ec7259, #ffea7f);
+  background-size: 400% 400%;
+  animation: Gradient 10s ease infinite;
+}
+
+.footer-wrap a {
+  color: #eee !important;
+}
+
+@keyframes Gradient {
+  0% {
+    background-position: 0 50%;
+  }
+  50% {
+    background-position: 100% 50%;
+  }
+  100% {
+    background-position: 0 50%;
+  }
 }
 </style>
